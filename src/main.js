883fe002f4f67d460140dbe27de1b1edbe1c4ae9
Farah <farah.yasin14@gmail.com>
2017-12-15 12:57:32 +1100

    remove unnecessary files

--- src/main.js
+++ src/main.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './containers/Root';
-import configureStore from './store/configureStore';
-import storage from './libs/storage';
 import './styles/main.scss'
 
-
-const APP_STORAGE = 'redux_kanban';
-
-const store = configureStore(storage.get(APP_STORAGE) || {});
-
-store.subscribe(() => {
-  if(!storage.get('debug')) {
-    storage.set(APP_STORAGE);
-  }
-});
-
 ReactDOM.render(
-  <Root store={store} />,
+  <Root />,
   document.getElementById('root')
 );
