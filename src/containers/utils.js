5a268a3eb9b146fdf625b218374ef2c4ea3117f8
Farah <farah.yasin14@gmail.com>
2017-12-15 15:03:33 +1100

    Change box colors, add highlighting.

--- src/containers/utils.js
+++ src/containers/utils.js
@@ -1,10 +1,8 @@
-
 // Source: http://www.robweir.com/blog/2010/02/microsoft-random-browser-ballot.html
 // Don't ask.
 const shuffle = list => {
     return list.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
 }
-
 
 const checkGameStatus = list => {
     for (let index = 0; index < list.length; index++) { 
@@ -15,8 +13,15 @@ const checkGameStatus = list => {
     return true
 }
 
-// Ouch. No time for fancy algorithm.
+// Ouch. No time for fancy algorithm. 
 // Ex. At position "0", only items [1, 4] are valid. Can get valid positions in 0(1).
+
+/* [ _,  1,   2,  3 ]
+/* [ 4,  5,   6,  7 ]
+/* [ 8,  9,  10, 11 ]
+/* [ 0,  12, 13, 14 ]
+*/
+
 var rules = [
     { "0": [1, 4]},{ "1": [0, 2, 5] },{ "2": [1, 3, 6] },{ "3": [2, 7] },
     { "4": [0, 5, 8] },{ "5": [1, 4, 6, 9] },{ "6": [2, 5, 7, 10] },{ "7": [3, 6, 11] },
