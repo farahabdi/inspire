e3c1b248b44c948159efb589c0b40ec1aad8c82c
Farah <farah.yasin14@gmail.com>
2017-12-18 13:00:45 +1100

    tell truth

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -6,7 +6,7 @@ const Panel = ({ shuffle, reset, boardStatus }) => {
     <div className='panel'>
       <a className='button' onClick={shuffle}><span>Shuffle</span></a>
       <a className='button' onClick={reset}><span>Reset</span></a>
-      <a className='button' disabled><span>{ boardStatus ? 'WIN' : 'LOSING'}</span></a>
+      <a className='button' disabled><span>{ boardStatus ? 'WINNER' : 'LOSER'}</span></a>
     </div>
   )
 }
